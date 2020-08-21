ffb71a4a22775f8ac0d0dbc9f37f13fb1351a0cf
迷渡 <justjavac@gmail.com>
2020-08-21 16:46:56 +0800

    add ftp status codes

--- status.ts
+++ status.ts
@@ -28,29 +28,29 @@ export const REQUESTED_FILE_ACTION_OK = /*              */ 250;
 export const PATH_CREATED = /*                          */ 257;
 
 // 3xx: Positive intermediate Reply
-export const NEED_PASSWORD = /*                            */ 331;
-export const LOGIN_NEED_ACCOUNT = /*                            */ 332;
-export const REQUEST_FILE_PENDING = /*                            */ 350;
+export const NEED_PASSWORD = /*                         */ 331;
+export const LOGIN_NEED_ACCOUNT = /*                    */ 332;
+export const REQUEST_FILE_PENDING = /*                  */ 350;
 
 // 4xx: Transient Negative Completion Reply
-export const NOT_AVAILABLE = /*                            */ 421;
-export const CANNOT_OPEN_DATA_CONNECTION = /*                            */ 425;
-export const TRANSER_ABORTED = /*                            */ 426;
-export const INVALID_CREDENTIALS = /*                            */ 430;
-export const HOST_UNAVAILABLE = /*                            */ 434;
-export const REQUEST_FILE_ACTION_IGNORED = /*                            */ 450;
-export const ACTION_ABORTED = 451;
-export const REQUESTED_ACTION_NOT_TAKEN = 452;
+export const NOT_AVAILABLE = /*                         */ 421;
+export const CANNOT_OPEN_DATA_CONNECTION = /*           */ 425;
+export const TRANSER_ABORTED = /*                       */ 426;
+export const INVALID_CREDENTIALS = /*                   */ 430;
+export const HOST_UNAVAILABLE = /*                      */ 434;
+export const REQUEST_FILE_ACTION_IGNORED = /*           */ 450;
+export const ACTION_ABORTED = /*                        */ 451;
+export const REQUESTED_ACTION_NOT_TAKEN = /*            */ 452;
 
 // 5xx: Permanent Negative Completion Reply
-export const BAD_COMMAND = /*                            */ 500;
-export const BAD_ARGUMENTS = /*                            */ 501;
-export const NOT_IMPLEMENTED = /*                            */ 502;
-export const BAD_SEQUENCE = /*                            */ 503;
-export const NOT_IMPLEMENTED_PARAMETER = /*                            */ 504;
-export const NOT_LOGGED_IN = /*                            */ 530;
-export const STORING_NEED_ACCOUNT = /*                            */ 532;
-export const FILE_UNAVAILABLE = /*                            */ 550;
-export const PAGE_TYPE_UNKNOWN = /*                            */ 551;
-export const EXCEEDED_STORAGE = /*                            */ 552;
-export const BAD_FILENAME = /*                            */ 553;
+export const BAD_COMMAND = /*                           */ 500;
+export const BAD_ARGUMENTS = /*                         */ 501;
+export const NOT_IMPLEMENTED = /*                       */ 502;
+export const BAD_SEQUENCE = /*                          */ 503;
+export const NOT_IMPLEMENTED_PARAMETER = /*             */ 504;
+export const NOT_LOGGED_IN = /*                         */ 530;
+export const STORING_NEED_ACCOUNT = /*                  */ 532;
+export const FILE_UNAVAILABLE = /*                      */ 550;
+export const PAGE_TYPE_UNKNOWN = /*                     */ 551;
+export const EXCEEDED_STORAGE = /*                      */ 552;
+export const BAD_FILENAME = /*                          */ 553;
